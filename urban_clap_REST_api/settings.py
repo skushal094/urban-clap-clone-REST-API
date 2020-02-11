540758d427710679d7b7fa93783cf7673f6ac614
Kushal Shah <skushal094@gmail.com>
2020-02-11 16:08:53 +0530

    settings modified

--- urban_clap_REST_api/settings.py
+++ urban_clap_REST_api/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework',
     'customuser',
+    'provider',
+    'customer',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +125,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'customuser.UserProfile'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', ),
+}
 
 try:
     from .local_settings import *
